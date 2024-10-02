cb7ced621057b80ca5bf5f5522e20bad1a51cb42
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2024-10-02 14:08:54 -0700

    update

--- testing.py
+++ testing.py
@@ -5,13 +5,15 @@ import cv2
 model = YOLO('yolov8n.pt')
 
 # define source
-source_cap = cv2.VideoCapture('traffic.mp4')
+source_cap = cv2.VideoCapture("assets/traffic.mp4")
 
 while True:
     ret, frame = source_cap.read()
-    # cv2.imshow('frame', frame)
-    # Inference
-    results = model(source=frame, show=True, save=False)
+    results = model(source=frame, show=False, save=False)
+    bbox = results.pandas().xyxy[0]
+    cls = results.pandas().xyxy[0].name
+    print(cls)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
